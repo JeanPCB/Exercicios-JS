0996a07174cb86cf4fa5233829713cce77675552
JeanPCB <jeanp.cebinel01@gmail.com>
2022-05-20 19:49:38 -0300

    Adição: maneira curta de capturar data

--- excs-pages/Aula-48/assets/js/main.js
+++ excs-pages/Aula-48/assets/js/main.js
@@ -1,61 +1,22 @@
-function setDate() {
+// MANEIRA CURTA:
+function setDateShort() {
     const mainSection = document.querySelector( '#main-section' );
-    const date = new Date();
-    const weekDay = setWeekDay( date.getDay() );
-    const day = leftZero( date.getDate() );
-    const month = setMonth( date.getMonth() );
-    const year = leftZero( date.getFullYear() );
-    const hours = leftZero( date.getHours() );
-    const minutes = leftZero( date.getMinutes() );
-    const seconds = leftZero( date.getSeconds() );
+    const data = new Date();
 
     function setFullDate() {
         const resultDiv = document.querySelector( '#result' );
         const p = createElement( 'p' );
-        p.textContent = getFullDate();
+        p.textContent = data.toLocaleString( 'pt-BR', {dateStyle: 'full'} );
         resultDiv.appendChild( p );
     }
 
     function setFullTime() {
         const resultDiv = document.querySelector( '#result' );
         const p = createElement( 'p' );
-        p.textContent = getFullTime();
+        p.textContent = data.toLocaleString( 'pt-BR', {timeStyle: 'short'} );
         resultDiv.appendChild( p );
     }
-
-    function getFullDate() {
-        return `${ weekDay }, ${ day } de ${ month } de ${ year }`;
-    }
-
-    function getFullTime() {
-        return `${ hours }:${ minutes }:${ seconds }`;
-    }
-
-/* Maneira mais sucinta ao invés de Switch */
-    function setMonth( month ) {
-        const months = [ 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Setembro', 'Outubro', 'Novembro', 'Dezembro' ];
-        return months[ month ];
-    }
-
-    function setWeekDay( day ) {
-        switch( day ) {
-            case 0:
-                return 'Domingo';
-            case 1:
-                return 'Segunda-feira';
-            case 2:
-                return 'Terça-feira';
-            case 3:
-                return 'Quarta-feira';
-            case 4:
-                return 'Quinta-feira';
-            case 5:
-                return 'Sexta-feira';
-            case 6:
-                return 'Sábado';
-        }
-    }
-
+    
     function divResult() {
         const newDiv = createElement( 'div' );
         const divLabel = createElement( 'p' );
@@ -63,13 +24,10 @@ function setDate() {
         divLabel.style = 'color: var(--primary-color); font-style: italic;';
         newDiv.setAttribute( 'id', 'result' );
         newDiv.appendChild( divLabel );
-        return mainSection.appendChild( newDiv );
+        mainSection.appendChild( newDiv );
     }
 
-    function leftZero( num ) {
-        return num >= 10 ? num : `0${ num }`;
-    }
-
+    
     function createElement( element ) {
         return document.createElement( element );
     }
@@ -79,4 +37,83 @@ function setDate() {
     setFullTime();
 }
 
-setDate();
+setDateShort();
+
+// MANEIRA LONGA:
+
+// function setDateLong() {
+//     const mainSection = document.querySelector( '#main-section' );
+//     const data = new Date();
+//     const weekDay = setWeekDay( data.getDay() );
+//     const day = leftZero( data.getDate() );
+//     const month = setMonth( data.getMonth() );
+//     const year = leftZero( data.getFullYear() );
+//     const hours = leftZero( data.getHours() );
+//     const minutes = leftZero( data.getMinutes() );
+//     const seconds = leftZero( data.getSeconds() );
+
+//     function setFullDate() {
+//         const resultDiv = document.querySelector( '#result' );
+//         const p = createElement( 'p' );
+//         p.textContent = getFullDate();
+//         resultDiv.appendChild( p );
+//     }
+
+//     function setFullTime() {
+//         const resultDiv = document.querySelector( '#result' );
+//         const p = createElement( 'p' );
+//         p.textContent = getFullTime();
+//         resultDiv.appendChild( p );
+//     }
+
+//     function getFullDate() {
+//         return `${ weekDay }, ${ day } de ${ month } de ${ year }`;
+//     }
+
+//     function getFullTime() {
+//         return `${ hours }:${ minutes }:${ seconds }`;
+//     }
+
+// /* Maneira mais sucinta ao invés de Switch */
+//     function setMonth( month ) {
+//         const months = [ 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Setembro', 'Outubro', 'Novembro', 'Dezembro' ];
+//         return months[ month ];
+//     }
+
+//     function setWeekDay( day ) {
+//         switch( day ) {
+//             case 0:
+//                 return 'Domingo';
+//             case 1:
+//                 return 'Segunda-feira';
+//             case 2:
+//                 return 'Terça-feira';
+//             case 3:
+//                 return 'Quarta-feira';
+//             case 4:
+//                 return 'Quinta-feira';
+//             case 5:
+//                 return 'Sexta-feira';
+//             case 6:
+//                 return 'Sábado';
+//         }
+//     }
+
+//     function divResult() {
+//         const newDiv = createElement( 'div' );
+//         newDiv.setAttribute( 'id', 'result' );
+//         return mainSection.appendChild( newDiv );
+//     }
+
+//     function leftZero( num ) {
+//         return num >= 10 ? num : `0${ num }`;
+//     }
+
+//     function createElement( element ) {
+//         return document.createElement( element );
+//     }
+
+//     divResult();
+//     setFullDate();
+//     setFullTime();
+// }
